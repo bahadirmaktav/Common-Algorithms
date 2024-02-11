017f2834658d30e9cf49420dc7c8d9942ff5c230
bahadirmaktav <bahadirmaktavv@gmail.com>
2024-02-12 01:02:22 +0300

    Add merge sort gtest code.

--- Sorting/SortTests.cpp
+++ Sorting/SortTests.cpp
@@ -1,9 +1,22 @@
 #include <gtest/gtest.h>
+#include "SortUtils.hpp"
+#include "MergeSort.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#define INPUT_LIST_SIZE 10
+
+TEST(MergeSortTest, SortingTest) {
+    std::vector<int> input_list;
+    SortUtils::GenerateInputList(input_list, INPUT_LIST_SIZE);
+
+    MergeSort merge_sort;
+    merge_sort.Sort(input_list);
+
+    for (size_t i = 0; i < input_list.size(); ++i) {
+        EXPECT_EQ(input_list[i], i + 1);
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
